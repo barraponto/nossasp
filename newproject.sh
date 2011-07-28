680220fab1a73f18c14f33bef72ef5387609028a
Capi Etheriel <barraponto@gmail.com>
2011-07-28 15:48:58 -0300

    added a script for new projects

---
+++ newproject.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#change the variables to suit your project
+PROJECT_EXTENDED="NossaSP: Mapa das Entidades Colaborativas"
+PROJECT="NossaSP"
+GITHUB_USER="barraponto"
+
+sed "s/tecidoSTARTER/$PROJECT/g" -i readme.md tecidoSTARTER.profile tecidoSTARTER-stub.make
+sed -i -e "s/^Tecido Starter/$PROJECT_EXTENDED/" -e "s/barraponto/$GITHUB_USER/" readme.md
+rename "s/tecidoSTARTER/$PROJECT/" *
